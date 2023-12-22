a4e5ab0c228a65e456b59ac685fa01f65590eded
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-22 13:37:24 +0100

    Update launcher.py

--- platformcode/launcher.py
+++ platformcode/launcher.py
@@ -24,7 +24,7 @@ def start():
                 changelog = fileC.read()
                 if changelog.strip() and config.get_setting("addon_update_message"):
                     platformtools.dialog_ok('Lo scenziato pazzo', 'Aggiornamenti applicati:\n' + changelog)
-            filetools.remove(config.changelogFile)
+            #filetools.remove(config.changelogFile)
                 elif changelog == 'update.txt'
                     platformtools.dialog_ok('Lo scenziato pazzo', 'Nuova Build disponibile vuoi installarla?')
         except:
