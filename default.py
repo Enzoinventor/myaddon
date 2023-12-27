0c02b0dd233e0bab534df6288909297858c45d44
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-27 03:02:26 +0100

    Update default.py

--- default.py
+++ default.py
@@ -28,22 +28,4 @@ from platformcode import launcher
 if sys.argv[2] == "":
     launcher.start()
 
-with open(config.updateFile, 'r') as fileC: # new function to control version
-                Update = fileC.readline()
-                logger.info("Versione:", Update)
-
-
-
 import default1 #build 
-
-#if Update ==('5.0.1'): #('/5\.0\.0/'):
-#    update_ok=platformtools.dialog_yesno("Lo Scienziato Pazzo","E' disponibile una nuova versione della build\nVuoi scaricarla?\nClicca su Build universale per installare gli aggiornamenti e attendi che il download sia completato\nKodi verrà riavviato e una volta aperto sarà aggiornato.")
-#    if update_ok:
-#        xbmc.executebuiltin("UpdateLocalAddons")
-#        xbmc.executebuiltin("StopScript(plugin.video.lo-scienziato-pazzo)")
-#        xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
-#        xbmc.executebuiltin("RunScript(special://home/addons/plugin.video.lo-scienziato-pazzo/default.py)")
-#    else :
-#        platformtools.dialog_ok("Lo Scienziato Pazzo","La Build è aggiornata")
-               
-         
