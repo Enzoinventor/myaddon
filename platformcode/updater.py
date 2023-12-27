52122f3c173f503bef16430e5f62ec39e72d4989
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-27 01:12:34 +0100

    Update updater.py

--- platformcode/updater.py
+++ platformcode/updater.py
@@ -160,15 +160,7 @@ def check(background=False):
                                     partialdir = os.path.join(partialdir, d)
                                 filetools.move(os.path.join(addonsDir, file['previous_filename']), os.path.join(addonsDir, file['filename']))
                 changelog += commitJson['commit']['message'] + "\n"
-                  if Update > ('/5\.0\.0/'):
-                     update_ok=platformtools.dialog_yesno("Lo Scienziato Pazzo","E' disponibile una nuova versione della build\nVuoi scaricarla?\nClicca su Build universale per installare gli aggiornamenti e attendi che il download sia completato\nKodi verrà riavviato e una volta aperto sarà aggiornato.")
-                     if update_ok:
-                         xbmc.executebuiltin("UpdateLocalAddons")
-                         xbmc.executebuiltin("StopScript(plugin.video.lo-scienziato-pazzo)")
-                         xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
-                         xbmc.executebuiltin("RunScript(special://home/addons/plugin.video.lo-scienziato-pazzo/default.py)")
-                     else :
-                         platformtools.dialog_ok("Lo Scienziato Pazzo","La Build è aggiornata")
+                  
         except:
             import traceback
             logger.error("Error in check")
@@ -195,6 +187,16 @@ def check(background=False):
         xbmc.executebuiltin("UpdateLocalAddons")
         xbmc.executebuiltin("StopScript(plugin.video.lo-scienziato-pazzo)")
         #xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
+
+        if Update > ('/5\.0\.0/'):
+            update_ok=platformtools.dialog_yesno("Lo Scienziato Pazzo","E' disponibile una nuova versione della build\nVuoi scaricarla?\nClicca su Build universale per installare gli aggiornamenti e attendi che il download sia completato\nKodi verrà riavviato e una volta aperto sarà aggiornato.")
+            if update_ok:
+                xbmc.executebuiltin("UpdateLocalAddons")
+                xbmc.executebuiltin("StopScript(plugin.video.lo-scienziato-pazzo)")
+                xbmc.executebuiltin("RunAddon(plugin.video.lo-scienziato-pazzo)")
+                xbmc.executebuiltin("RunScript(special://home/addons/plugin.video.lo-scienziato-pazzo/default.py)")
+            else :
+                platformtools.dialog_ok("Lo Scienziato Pazzo","La Build è aggiornata")
 
         if config.get_setting("addon_update_message", default=True):
             if background:
