a80bd3dc45f48fcf4c3c91bbbdb911b657f5ed7b
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-28 21:11:30 +0100

    Update updater.py

--- platformcode/updater.py
+++ platformcode/updater.py
@@ -8,7 +8,7 @@ from platformcode import config, logger, platformtools, filetools
 import json
 import xbmc
 import re
-import request
+import requests
 from lib import githash
 try:
     import urllib.request as urllib
