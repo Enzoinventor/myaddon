6260b54ef1758799928ce082e34c0a0161ec635a
aandroide <14141574+aandroide@users.noreply.github.com>
2023-12-28 22:57:29 +0100

    Update updater.py

--- platformcode/updater.py
+++ platformcode/updater.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from lib.six import BytesIO
 from platformcode import config, logger, platformtools, filetools
-from default1 import downloader
+import default1 as wizard
 import json
 import xbmc
 import re
