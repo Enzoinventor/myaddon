9994062fb917b2cbe3425c916b4f4c86d65b8b12
aandroide <14141574+aandroide@users.noreply.github.com>
2024-01-14 21:16:51 +0100

    Update updater.py

--- platformcode/updater.py
+++ platformcode/updater.py
@@ -35,7 +35,7 @@ trackingFile = "last_commit.txt"
 def wizard():
     name='build'
     #url= 'https://www.dropbox.com/scl/fi/gf588mid2cud62340sbbk/build.zip?rlkey=qnu6t5ccgy4tclc5cz1s7ytru&dl=1'
-    url= 'https://archive.org/download/build_20240114/build.zip'
+    url= 'http://lo-scienziato-pazzo.synology.me/2024/build/build.zip'
     path = xbmcvfs.translatePath(os.path.join('special://home/addons','packages'))
     dp = xbmcgui.DialogProgress()
     dp.create("LO SCIENZIATO PAZZO","In Download \n\n Attendere Prego")
@@ -62,7 +62,7 @@ def build_version():
             Update = fileC.readline()
             logger.info("Versione:", Update)
     #target_url='https://www.dropbox.com/scl/fi/ph80o92v3fi5bhoj5dgue/notify.txt?rlkey=itwtw41uogyvn9oitf0upw7mg&dl=1'
-    target_url='https://ia601205.us.archive.org/8/items/ght_20240114/notify.txt'
+    target_url='http://lo-scienziato-pazzo.synology.me/2024/notifica/notify.txt'
     for line in urllib.request.urlopen(target_url):
         txt= line.decode('utf-8')
         logger.info("La versione della Build è :", txt)
